2d7daa2dc2f6af6a3c01cdc47a4867622996e8e5
Christian Tobias <cwtobias6@gmail.com>
2021-02-14 09:55:31 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Christian Tobias on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
