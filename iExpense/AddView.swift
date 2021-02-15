204d39c7c1b36ccead25d829959be2691428d201
Christian Tobias <cwtobias6@gmail.com>
2021-02-14 16:32:41 -0800

    List Setup

---
+++ iExpense/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Christian Tobias on 2/14/21.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+struct ExpenseItem:Identifiable {
+    let id = UUID()
+    var name:String
+    var type:String
+    var amount:Int
+}
+
+class Expenses:ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items) {Text($0.name)}
+                    .onDelete(perform: removeItems)
+            }
+            
+            .navigationTitle("iExpense")
+            .navigationBarItems(trailing: Button(action: {
+                let expense = ExpenseItem(name: "Xbox \(Int.random(in: 0...10))", type: "Gaming", amount: 10)
+                expenses.items.append(expense)
+                
+            }, label: {
+                Image(systemName: "plus")
+            }))
+        }
+    }
+    
+    func removeItems(at offsets:IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
